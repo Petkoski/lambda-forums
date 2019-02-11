fd57f9e6ea6a6328d69c05149b08e83619b88c40
Petkoski <petkoski15@hotmail.com>
2019-02-11 20:50:59 +0100

    Initial commit, create data project

--- LambdaForums/Startup.cs
+++ LambdaForums/Startup.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using LambdaForums.Data;
-using LambdaForums.Models;
+using LambdaForums.Data.Models;
 using LambdaForums.Services;
 
 namespace LambdaForums
